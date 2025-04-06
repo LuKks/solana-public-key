aee087ac35e8ef55831c06d68778ec77ed19c85f
Lucas Barrena <lucasbarrena98@gmail.com>
2025-04-06 20:42:15 -0300

    First commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# solana-public-key
+
+```
+npm i solana-public-key
+```
+
+## Usage
+
+```js
+const solana-public-key = require('solana-public-key')
+```
+
+## License
+
+MIT
---
+++ index.js
@@ -0,0 +1,92 @@
+const crypto = require('crypto')
+const { default: bs58 } = require('bs58')
+const ed25519 = require('@noble/ed25519')
+
+const PROGRAM_DERIVED_ADDRESS = Buffer.from('ProgramDerivedAddress')
+
+const INSPECT = Symbol.for('nodejs.util.inspect.custom')
+
+module.exports = class PublicKey {
+  constructor (input) {
+    if (typeof input === 'string') {
+      this.bytes = bs58.decode(input)
+    } else if (Buffer.isBuffer(input) || input instanceof Uint8Array) {
+      this.bytes = Buffer.from(input)
+    } else if (input instanceof PublicKey) {
+      this.bytes = input.bytes
+    } else {
+      throw new Error('Invalid input')
+    }
+
+    if (this.bytes.length !== 32) {
+      throw new Error('Invalid length')
+    }
+  }
+
+  [INSPECT] () {
+    return this.constructor.name + '(' + this.toBase58() + ')'
+  }
+
+  toString () {
+    return this.toBase58()
+  }
+
+  toBuffer () {
+    return Buffer.from(this.bytes)
+  }
+
+  toBase58 () {
+    return bs58.encode(this.bytes)
+  }
+
+  equals (other) {
+    const b = other instanceof PublicKey ? other : new PublicKey(other)
+
+    return this.bytes.equals(b.bytes)
+  }
+
+  isOnCurve () {
+    return PublicKey.isOnCurve(this.bytes)
+  }
+
+  static createProgramAddressSync (seeds, programId) {
+    const buffer = Buffer.concat([
+      ...seeds,
+      new PublicKey(programId).toBuffer(),
+      PROGRAM_DERIVED_ADDRESS
+    ])
+
+    const hash = crypto.createHash('sha256').update(buffer).digest()
+
+    if (PublicKey.isOnCurve(hash)) {
+      throw new Error('Invalid seeds, address must not be on curve')
+    }
+
+    return new PublicKey(hash)
+  }
+
+  static findProgramAddressSync (seeds, programId) {
+    for (let bump = 255; bump >= 0; bump--) {
+      try {
+        const seedsWithBump = seeds.concat(Buffer.from([bump]))
+        const pda = PublicKey.createProgramAddressSync(seedsWithBump, programId)
+
+        return [pda, bump]
+      } catch {
+        // Try next bump
+      }
+    }
+
+    throw new Error('Unable to find a valid program address')
+  }
+
+  static isOnCurve (buffer) {
+    try {
+      ed25519.ExtendedPoint.fromHex(buffer)
+
+      return true
+    } catch {
+      return false
+    }
+  }
+}
